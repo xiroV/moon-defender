7f4b696db7530ba0223673500a9b8efea93a8e90
Brian Alberg <brian@alberg.org>
2020-11-09 19:16:56 +0100

    Added initial work

---
+++ player.lua
@@ -0,0 +1,22 @@
+player = {}
+
+function player:init()
+
+end
+
+function player:draw()
+
+end
+
+function player:update()
+
+end
+
+function player:keypressed()
+
+end
+
+function player:keyreleased()
+
+end
+
