4997797b8219a2e084e305151093ea0ca58733e7
Brian Alberg <brian@alberg.org>
2020-11-10 23:54:44 +0100

    Refactoring

--- main.lua
+++ main.lua
@@ -1,36 +1,25 @@
-function love.update(dt)
+require "player"
 
+function love.update(dt)
+    player:update(dt)
 end
 
 function love.draw()
-    playerWidth = 20
-    playerHeight = 40
     love.graphics.setColor(0.8, 0.8, 0.8)
-    love.graphics.circle("fill", windowWidth/2, windowHeight*2-200, windowHeight, 500)
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.rectangle("fill", windowWidth/2-playerWidth/2, windowHeight-200-playerHeight, playerWidth, playerHeight)
-
-    love.graphics.setColor(0.2, 0.3, 0.2)
-    love.graphics.line(windowWidth/2, windowHeight-200-playerHeight+10, love.mouse.getX(), love.mouse.getY())
-
-    local p = {x=windowWidth/2, y=windowHeight-200-playerHeight+10}
-    local m = {x=love.mouse.getX(), y=love.mouse.getY()}
-
-    angle = math.atan2((m.y - p.y), (m.x - p.x))
-
-    love.graphics.translate(windowWidth/2, windowHeight-200-playerHeight+10)
-    love.graphics.setColor(0.7, 0.7, 0.7)
-    love.graphics.draw(gun, 0, 0, angle, 1, 1, 0, 5)
+    love.graphics.circle("fill", window.width/2, window.height*2-200, window.height, 500)
+    player:draw()
 end
 
 function love.load()
     math.randomseed(os.time())
-    windowHeight = 800
-    windowWidth = 1280
+    window = {width=1280, height=800}
     love.window.setTitle("Moon Defender")
-    love.window.setMode(windowWidth, windowHeight)
+    love.window.setMode(window.width, window.height)
 
     gun = love.graphics.newImage("assets/gun.png")
+
+    player:load()
+
 end
 
 function love.mousepressed(_, _, button, istouch, presses)
--- player.lua
+++ player.lua
@@ -1,15 +1,34 @@
 player = {}
 
-function player:init()
+function player:load()
+    player.width = 20
+    player.height = 40
+    player.aimAngle = 0
 
+    player.sprite = {
+        gun = love.graphics.newImage("assets/gun.png")
+    }
 end
 
 function player:draw()
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.rectangle("fill", window.width/2-player.width/2, window.height-200-player.height, player.width, player.height)
 
+    love.graphics.setColor(0.2, 0.3, 0.2)
+    love.graphics.line(window.width/2, window.height-200-player.height+10, love.mouse.getX(), love.mouse.getY())
+
+    love.graphics.translate(window.width/2, window.height-200-player.height+10)
+    love.graphics.setColor(0.7, 0.7, 0.7)
+    love.graphics.draw(player.sprite.gun, 0, 0, player.aimAngle, 1, 1, 0, 5)
 end
 
-function player:update()
+function player:update(dt)
+    local p = {x=window.width/2, y=window.height-200-player.height+10}
+    local m = {x=love.mouse.getX(), y=love.mouse.getY()}
 
+    player.aimAngle = math.atan2((m.y - p.y), (m.x - p.x))
+
+   
 end
 
 function player:keypressed()
